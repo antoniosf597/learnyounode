9a5e0845dec5eb177a06288351bab5d634e407f7
Antonio Sanchez <antoniosf597@gmail.com>
2016-10-03 08:51:21 +0200

    Mi primer archivo Asincrono

---
+++ myFirstAsyncIO.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+fs.readFile(process.argv[2],
+	function (error, buffer) {
+		if (error) {
+			console.log(error);
+		} else {
+			var string = buffer.toString();
+
+			var arrayLineas = string.split('\n');
+
+			console.log(arrayLineas.length - 1);
+		}
+	});
