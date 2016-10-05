f2bf68b0b33e132d3647522d1c34de16820348e4
Antonio Sanchez <antoniosf597@gmail.com>
2016-10-05 10:36:58 +0200

    Ejercicio HTTP client

---
+++ httpClient.js
@@ -0,0 +1,16 @@
+
+var http = require('http');
+
+http.get(process.argv[2], function(res) {
+  	res.setEncoding('utf8');
+	res.on("data", function (data) { 
+		console.log(data);
+	})
+	res.on("error", function (data) { 
+		console.log('Error:' + error.message);
+	})
+
+
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
